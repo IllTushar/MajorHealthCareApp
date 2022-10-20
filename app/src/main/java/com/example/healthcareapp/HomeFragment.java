d6d64fd78e70e4dfa7ab7aa1dbd51816332cc5c4
lllTushar <gtushar7668@gmail.com>
2022-10-20 23:38:31 +0530

    Fourth Commit

--- app/src/main/java/com/example/healthcareapp/HomeFragment.java
+++ app/src/main/java/com/example/healthcareapp/HomeFragment.java
@@ -1,64 +1,95 @@
 package com.example.healthcareapp;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+TextView name,gmail,profile;
+EditText password;
+TextInputLayout layout;
+FloatingActionButton floatingActionButton;
+FirebaseDatabase database;
+DatabaseReference reference;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        name = root.findViewById(R.id.name);
+        gmail = root.findViewById(R.id.gmail);
+        profile = root.findViewById(R.id.profile);
+        password = root.findViewById(R.id.password);
+        layout = root.findViewById(R.id.textinputlayout);
+        floatingActionButton = root.findViewById(R.id.btnfloating);
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("Details");
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProgressDialog pd = new ProgressDialog(getContext());
+                pd.setMessage("Loading..");
+                pd.show();
+                String Password =password.getText().toString();
+                if (password.getText().toString().isEmpty()){
+                    Toast.makeText(getContext(), "Enter Password!!", Toast.LENGTH_SHORT).show();
+                    pd.dismiss();
+                }else{
+                    reference.child(Password).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            pd.dismiss();
+                            profile.setVisibility(View.GONE);
+                            password.setVisibility(View.GONE);
+                            layout.setVisibility(View.GONE);
+                            floatingActionButton.setVisibility(View.GONE);
+                            String Email = String.valueOf(snapshot.child("email").getValue());
+                            String Name = String.valueOf(snapshot.child("name").getValue());
+                            name.setVisibility(View.VISIBLE);
+                            name.setText(Name);
+                            gmail.setVisibility(View.VISIBLE);
+                            gmail.setText(Email);
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            pd.dismiss();
+                            Toast.makeText(getContext(), "Something went wrong !!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+            }
+        });
+
+        return root;
     }
 }
